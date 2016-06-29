d784ffab5964ad9bb440b031d4a8aced4bc1adab
Paul Tierney <Paul@adavir.ie>
2016-06-29 10:57:01 +0100

    Finished POC example

--- MapPlotter.UI.Web/App_Start/BundleConfig.cs
+++ MapPlotter.UI.Web/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace MapPlotter.UI.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-
+            bundles.Add(new ScriptBundle("~/bundles/lib").Include(
+                      "~/Scripts/dijkstra.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
